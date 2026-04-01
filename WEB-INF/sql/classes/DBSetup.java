f4579637715ddbfd7adbc6b1b9036ecf04411e8c
Zartox Flox <darkfireyo@gmail.com>
2026-04-01 16:52:34 +0200

    frontend + setupDB user

---
+++ WEB-INF/sql/classes/DBSetup.java
@@ -0,0 +1,13 @@
+package WEB-INF.sql.classes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+public class DBSetup {
+    // Configuration de la base de données PostgreSQL
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USER = "amin";
+    private static final String DB_PASSWORD = "amin";
+
+}
