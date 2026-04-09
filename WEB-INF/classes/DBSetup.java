ec77ae30204b627abc95835a919422d32107b8e9
Ayoub <125963399+Zenkai04@users.noreply.github.com>
2026-04-09 12:09:51 +0200

    fix champs

--- WEB-INF/classes/DBSetup.java
+++ WEB-INF/classes/DBSetup.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class DBSetup {
     // Configuration de la base de données PostgreSQL
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String DB_USER = "amin";
-    private static final String DB_PASSWORD = "amin";
+    private static final String DB_USER = "ayoub";
+    private static final String DB_PASSWORD = ".Zenkai040103";
 
 }
